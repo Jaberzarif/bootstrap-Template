1d499e8b77131ef0ac8f648d37d2c74315733f6f
Jaber Zarif <jaber.zarif@gmail.com>
2017-05-14 05:07:19 +0200

    first commit

---
+++ js/animate.js
@@ -0,0 +1,13 @@
+var tl = new TimelineLite, 
+    mySplitText = new SplitText("#quote", {type:"words,chars"}), 
+    chars = mySplitText.chars; //an array of all the divs that wrap each character
+
+TweenLite.set("#quote", {perspective:400});
+
+tl.staggerFrom(chars, 0.8, {opacity:0, scale:0, y:80, rotationX:180, transformOrigin:"0% 50% -50",  ease:Back.easeOut}, 0.01, "+=0");
+
+
+
+document.getElementById("animate").onclick = function() {
+  tl.restart();
+}
